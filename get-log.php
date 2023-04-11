5df4bcdc673e5f3f88ab53b79d20849ec045b515
SimoneFerrante <simoneferrante2405@gmail.com>
2023-04-11 12:13:59 +0200

    Modifiche varie

--- get-log.php
+++ get-log.php
@@ -1,15 +1,28 @@
 <?php
-  $username = mysqli_real_escape_string($db, $_POST["nickname"]);
-  $pass = mysqli_real_escape_string($db, $_POST["password"]);
-  $sql = "SELECT id,nickname FROM users WHERE nickname = ? AND password = SHA2(CONCAT(?, (SELECT salt FROM users WHERE nickname = ?)), 256);";
-  $stmt = $db->prepare($sql);
-  
-  $stmt->bind_param("sss", $nickname, $password, $nickname);
-  $stmt->bind_result($nickname);
-  $stmt->execute();
-  if ($stmt->fetch())
-    echo "<p>Bentornato: {$nickname}</p>";
-  else
-    echo "<p>Login Fallito</p>";
-  $stmt->close();
+require 'db_connection.php';
+session_start();
+
+$nickname = mysqli_real_escape_string($connection, $_POST["nickname"]);
+$password = mysqli_real_escape_string($connection, $_POST["password"]);
+
+$sql="SELECT id,nickname FROM users WHERE nickname=? AND password=SHA2(CONCAT(?, (SELECT salt FROM users WHERE nickname=?)), 256);";
+
+$statement = $connection->prepare($sql);
+
+$statement->bind_param("sss", $nickname, $pass, $nickname);
+$query_response = [];
+$statement->bind_result($query_response['id'], $query_response['nickname']);
+$statement->execute();
+
+if ($statement->fetch()){
+  $_SESSION['user_id'] = $query_response['id'];
+  $_SESSION['nickname'] = $query_response['nickname'];
+  $response['message'] = 'Login Riuscito';
+  header('Location: home.php');
+}else{
+  http_response_code(400);
+  $response['message'] = 'Login Fallito';
+}
+$statement->close();
+echo json_encode($response);
 ?>
