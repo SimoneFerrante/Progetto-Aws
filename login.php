f6908b4b2853057192245c1a7b8840540b589058
SimoneFerrante <simoneferrante2405@gmail.com>
2023-04-11 14:36:20 +0200

    add login.css

--- login.php
+++ login.php
@@ -5,11 +5,13 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/login.css">
         <title>Login</title>
     </head>
     <body>
         <div class="container">
             <form action="get-log.php" method="post">
+                <h1>Login</h1>
                 <div class="form-group">
                     <input type="text" name="nickname" placeholder="Nickname" class="form-control" required/>
                 </div>
